2cadb1bd1059ac68b5ce4d4c88e36e2a07233e52
Miguel Hugo <dragonmike6@gmail.com>
2022-09-09 15:28:33 -0500

    added feature verification

--- automationWappi/src/test/java/com/speedup/qa/stepdefinition/updateProfileStepDefinition.java
+++ automationWappi/src/test/java/com/speedup/qa/stepdefinition/updateProfileStepDefinition.java
@@ -1,12 +1,14 @@
 package com.speedup.qa.stepdefinition;
 
 import com.speedup.qa.models.*;
+import com.speedup.qa.questions.UpdateProfileSaveButtonQ;
 import com.speedup.qa.tasks.*;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
@@ -19,6 +21,7 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 import static com.speedup.qa.userinterface.UpdateProfile.MODIFY_COUNTRY;
+import static com.speedup.qa.userinterface.UpdateProfile.SELECT_PHOTO;
 import static net.serenitybdd.screenplay.actors.OnStage.setTheStage;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
@@ -49,7 +52,7 @@ public class updateProfileStepDefinition {
     }
 
     @And("^change the '\"([^\"]*)\"', '\"([^\"]*)\"', '\"([^\"]*)\"', '\"([^\"]*)\"', \"([^\"]*)\"$")
-    public void changeThe(UpdateDataName firstName, UpdateDataLastName lastName, UpdateDataBirth birth, UpdateDataCountry country, String arg5) {
+    public void changeThe(UpdateDataName firstName, UpdateDataLastName lastName, UpdateDataBirth birth, UpdateDataCountry country, UpdateDataSexo sex) {
         OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileName.updateProfile(firstName));
         OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileLastName.updateProfileLastName(lastName));
         OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileBirth.withTheData(birth));
@@ -64,12 +67,16 @@ public class updateProfileStepDefinition {
         } else if (eua.equals(country.getCountry())){
             menu1.selectByIndex(3);
         }
-
-        //OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfilePhoto.profilePhoto());
+        OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileSexo.withTheDataSex(sex));
+        myBrowser.findElement(SELECT_PHOTO).sendKeys("C:\\Users\\drago\\OneDrive\\Escritorio\\unknown.png");
     }
 
     @Then("^we save the changes (.*)$")
     public void weSaveTheChanges(String save) {
-       // OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileSave.updateProfileSave());
+        OnStage.theActorInTheSpotlight().attemptsTo(UpdateProfileSave.updateProfileSave());
+        if (save == save) {
+            OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(UpdateProfileSaveButtonQ.messageSaveButton(), org.hamcrest.Matchers.is(save)));
+            System.out.println("The information was saved correctly");
+        }
     }
 }
