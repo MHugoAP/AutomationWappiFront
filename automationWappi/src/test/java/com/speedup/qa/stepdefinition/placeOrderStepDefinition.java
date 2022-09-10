567eba39bef3d667402c585fbac96295cb6eb1e2
Miguel Hugo <dragonmike6@gmail.com>
2022-09-10 15:41:59 -0500

    we improve the defined steps of our critical path

--- automationWappi/src/test/java/com/speedup/qa/stepdefinition/placeOrderStepDefinition.java
+++ automationWappi/src/test/java/com/speedup/qa/stepdefinition/placeOrderStepDefinition.java
@@ -2,26 +2,14 @@ package com.speedup.qa.stepdefinition;
 
 import com.speedup.qa.questions.PlaceOrderConfirmer;
 import com.speedup.qa.questions.PlaceOrderCopyCoupon;
-import com.speedup.qa.questions.UpdateProfileSaveButtonQ;
-import com.speedup.qa.tasks.Close;
-import com.speedup.qa.tasks.PlaceOrderBuyCoupon;
-import com.speedup.qa.tasks.PlaceOrderGetCoupon;
-import com.speedup.qa.tasks.PlaceOrderWithoutCoupon;
+import com.speedup.qa.tasks.*;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
-import static com.speedup.qa.userinterface.PlaceOrder.COPY_COUPON;
-import static com.speedup.qa.userinterface.PlaceOrder.COUPY_COUPON_VERSION2;
-import static com.speedup.qa.userinterface.PlaceOrderInitiation.PASTE_COUPON2;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class placeOrderStepDefinition {
@@ -29,12 +17,11 @@ public class placeOrderStepDefinition {
     private WebDriver myBrowser;
 
     @Given("^I want to place an order with the discount coupon$")
-    public void iWantToPlaceAnOrderWithTheDiscountCoupon() throws InterruptedException {
-        Actions actions = new Actions(myBrowser);
+    public void iWantToPlaceAnOrderWithTheDiscountCoupon(){
         OnStage.theActorInTheSpotlight().attemptsTo(PlaceOrderGetCoupon.placeOrdertGetCoupon());
         System.out.println("Your coupon is: " + PlaceOrderCopyCoupon.messageCopy().answeredBy(theActorInTheSpotlight()));
         OnStage.theActorInTheSpotlight().attemptsTo(PlaceOrderBuyCoupon.placeOrdertBuyCoupon());
-
+        OnStage.theActorInTheSpotlight().attemptsTo(PlaceOrderVerifyCoupon.placeOrderVerifyCoupon());
     }
 
     @And("^place another order without the coupon$")
